9972605e998ab4d634ec9daa275f2c4c8a9ecde7
Edwin Rodriguez <Edwin Rodriguez>
2016-08-28 14:13:03 -0400

    replase letter with -

---
+++ letter.js
@@ -0,0 +1,14 @@
+phrase = 'hello world';
+
+    for (i = 0; i < phrase.length; i++) {
+
+        if (phrase[i] == ' ') {
+
+            console.log('-');
+
+
+        } else {
+
+            console.log('_');
+        }
+    }
